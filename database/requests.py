7c041737d42a33d02f60d0597a8ed3f536c36713
R3al1ty1 <dolfinikkonovalov@gmail.com>
2024-12-06 17:26:30 +0300

    db hotfix

--- database/requests.py
+++ database/requests.py
@@ -10,7 +10,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = f"postgresql://{os.getenv('DB-USER')}:{os.getenv('DB-PASSWORD')}@{os.getenv('DB-HOST')}:{os.getenv('DB-PORT')}/{os.getenv('DB-NAME')}"
+DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 Base.metadata.create_all(engine)
